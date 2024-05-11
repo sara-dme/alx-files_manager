31c6c9d009f0ad301090dafe189a363f5ce19289
sara-dme <s.daoumy@gamil.com>
2024-05-11 18:46:07 +0100

    task 4 and 3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController').default;
+import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-router.get('/status', (req, res) => {
-     AppController.getStatus(req, res);
+router.get('/status', (req, res) => {AppController.getStatus(req, res);
 });
-router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
+router.get('/stats', (req, res) => {AppController.getStats(req, res);
 });
+router.get('/users', UserController.postNew);
+router.get('/users/me', UserController.getMe);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
 
 module.exports = router;
